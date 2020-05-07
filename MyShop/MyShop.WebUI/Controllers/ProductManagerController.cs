a7543d7648b9359b66c8b7eefe7d9f79310caa2e
vickyrajpoot <vickyrajpoot@VICKY>
2020-05-07 03:29:05 -0700

    Changes made to Products and Models

---
+++ MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyShop.Core.Models;
+
+namespace MyShop.WebUI.Controllers
+{
+    public class ProductManagerController : Controller
+    {
+       
+        // GET: ProductManager
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
